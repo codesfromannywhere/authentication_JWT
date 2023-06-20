5a57b62b7ef4b40574f600048c5ca958129cb2df
codesfromannywhere <codesfromannywhere@gmail.com>
2023-06-20 14:55:17 +0200

    added /secure route and cookies

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { UserModel } from "./model/index.js";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+
 
 
 
@@ -13,6 +16,7 @@ const ReactAppDistPath = new URL("../front-end/dist/", import.meta.url);
 const ReactAppIndex = new URL("../front-end/dist/index.html", import.meta.url);
 
 app.use(express.json());
+app.use(cookieParser())
 app.use(express.static(ReactAppDistPath.pathname));
 
 
@@ -60,44 +64,42 @@ app.post("/api/login", async (req, res) => {
 
     // Überprüft PW
     const isPasswordValid = user.verifyPassword(password);
-
-    const token = user.generateAuthToken();
-    res.json(token);
-
     if (!isPasswordValid) {
       return res.status(401).json({ message: "Ungültige E-mail oder Passwort" });
     }
-    res.status(200).json({ message: "Login erfolgreich" });
+
+    const token = user.generateAuthToken({ email });
+    res.cookie("auth", token, { httpOnly: true, maxAge: 1000 * 60 * 30 }); // Cookie
+    return res.status(200).json({ message: "Login erfolgreich", data: { token } });
+
   } catch (error) {
     res.status(500).json({ message: "Login fehlgeschlagen" });
   }
 });
 
 
-// const authenticateToken = (req, res, next) => {
-//   const authHeader = req.headers["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1]; // Extrahiere den Token aus dem Authorization Header
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Extrahiere den Token aus dem Authorization Header
 
-//   if (!token) {
-//     return res.sendStatus(401); // Token nicht vorhanden
-//   }
+  if (!token) {
+    return res.sendStatus(401); // Token nicht vorhanden
+  }
 
-//   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-//     if (err) {
-//       return res.sendStatus(403); // Token ungültig oder abgelaufen
-//     }
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // Token ungültig oder abgelaufen
+    }
 
-//     req.user = user; // Speichere den Benutzer aus dem Token im Request-Objekt
-//     next(); // Rufe die nächste Middleware oder den Controller auf
-//   });
-// };
-
-
+    req.user = user; // Speichere den Benutzer aus dem Token im Request-Objekt
+    next(); // Rufe die nächste Middleware oder den Controller auf
+  });
+};
 
 
-// app.get("/api/secure", authenticateToken, (req, res) => {
-//   res.json({ authenticated: true });
-// });
+app.get("/api/secure", authenticateToken, (req, res) => {
+  res.json({ authenticated: true });
+});
 
 
 
